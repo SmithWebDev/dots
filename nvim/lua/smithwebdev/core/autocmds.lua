b364e22312de73b982e7ae7eef60a9513d73bd57
Arnold Chand <creativenull@outlook.com>
2021-10-18 20:43:13 -0400

    feat(core): add ft for textchanged event

--- nvim/lua/smithwebdev/core/autocmds.lua
+++ nvim/lua/smithwebdev/core/autocmds.lua
@@ -34,13 +34,11 @@ auto(
   [[
     augroup autosave
         au!
-        let blacklist = ['packer']
+        let blacklist = ['packer', 'NvimTree', 'netrw', 'TelescopePrompt', 'spectre_panel', 'startify', 'calendar', 'text', 'help', 'Trouble', 'tutor', 'Neoformat', 'lspinfo', 'any-jump', 'tsplayground', 'orgagenda', 'gina-branch', 'lazygit','LspSagaCodeAction']
         au BufEnter * if &filetype == "" | setlocal ft=text | endif
         au TextChanged,InsertLeave * if index(blacklist, &ft) < 0 | silent w | endif
     augroup END
   ]], false
   )
 
-        --let blacklist = ['packer', 'NvimTree', 'netrw', 'TelescopePrompt', 'spectre_panel', 'startify', 'calendar', 'text', 'help', 'Trouble', 'tutor', 'Neoformat', 'lspinfo', 'any-jump', 'tsplayground', 'orgagenda', 'gina-branch', 'lazygit','LspSagaCodeAction']
-
--- print('Autocommands connected')
+-- let blacklist = ['packer', 'NvimTree', 'netrw', 'TelescopePrompt', 'spectre_panel', 'startify', 'calendar', 'text', 'help', 'Trouble', 'tutor', 'Neoformat', 'lspinfo', 'any-jump', 'tsplayground', 'orgagenda', 'gina-branch', 'lazygit','LspSagaCodeAction']
