9ad05c2f809bc19b97c8960aeb8fcc8fd0f11818
Darrick Smith <smithwebdd@gmail.com>
2021-10-21 20:59:13 -0400

    added rails config/setup to project

--- nvim/lua/smithwebdev/plugins/config/rails.lua
+++ nvim/lua/smithwebdev/plugins/config/rails.lua
@@ -4,24 +4,24 @@ M.plugin = {
   'tpope/vim-rails',
 
   requires = {
-    'tpope/vim-endwise',             --,       https://github.com/tpope/vim-endwise
-    'joshcheek/seeing_is_believing', --,       https://github.com/JoshCheek/seeing_is_believing
-    'tpope/vim-dispatch',            --,       https://github.com/tpope/vim-dispatch
-    'tpope/vim-projectionist', --,             https://github.com/tpope/vim-projectionist
+    'tpope/vim-endwise',             --,                https://github.com/tpope/vim-endwise
+    'joshcheek/seeing_is_believing', --,                https://github.com/JoshCheek/seeing_is_believing
+    'tpope/vim-dispatch',            --,                https://github.com/tpope/vim-dispatch
+    'tpope/vim-projectionist', --,                      https://github.com/tpope/vim-projectionist
   },
 
   after = 'mapx.nvim',
 
   config = function()
     -- keybindings
-    nnoremap('<leader>rba', '!bundle add ')
-    nnoremap('<leader>rbi', '!bundle install<CR>')
-    nnoremap('<leader>rc', '<cmd>Console!<CR>')
-    nnoremap('<leader>rs', '<cmd>Server!<CR>')
+    nnoremap('<leader>rba', ':!bundle add<Space>') --,  Bundle Add Command
+    nnoremap('<leader>rbi', ':!bundle install<CR>') --, Bundle Install Command
+    nnoremap('<leader>rc', '<cmd>Console!<CR>') --,     Open Rails Console New Tmux Window
+    nnoremap('<leader>rs', '<cmd>Server!<CR>') --,      Open Rails Server New Tmux Window
 
 
-    nnoremap('<leader>rg', '<cmd>Generate ')
-    nnoremap('<leader>rd', '<cmd>Destroy ')
+    nnoremap('<leader>rg', ':Generate<Space>') --,      Rails Generate Command
+    nnoremap('<leader>rd', ':Destroy<Space>') --,       Rails Destroy Command
 
     -- Invoking :Rails with no arguments runs a default task.
     nnoremap('<leader>rR', '<cmd>Rails<CR>')
@@ -40,38 +40,38 @@ M.plugin = {
 
 
 
-    -- Current file,           Alternate file(:A),   Related file(:R)
-    -- model,                  unit test,            schema definition
-    -- controller (in method), functional test,      template (view)
-    -- template (view),        functional test,      controller (jump to method)
-    -- migration,              previous migration,   next migration
-    -- database.yml,           database.example.yml, environments/*.rb
+    -- Current file            Alternate file(:A),   Related file(:R)
+    -- model                   unit test,            schema definition
+    -- controller (in method)  functional test,      template (view)
+    -- template (view)         functional test,      controller (jump to method)
+    -- migration               previous migration,   next migration
+    -- database.yml            database.example.yml, environments/*.rb
 
     -- Edits to "Alternative" file in various locations
     nnoremap('<leader>ra', '<cmd>A<CR>')
-    nnoremap('<leader>rae', '<cmd>AE<CR>') --, 'edit alternate file'
-    nnoremap('<leader>ras', '<cmd>AS<CR>') --, 'edit alternate in h split'
-    nnoremap('<leader>rav', '<cmd>AV<CR>') --, 'edit alternate in tab'},
-    nnoremap('<leader>rat', '<cmd>AT<CR>') --, 'edit alternate in v split'
+    nnoremap('<leader>rae', '<cmd>AE<CR>') --,          'edit alternate file'
+    nnoremap('<leader>ras', '<cmd>AS<CR>') --,          'edit alternate in h split'
+    nnoremap('<leader>rav', '<cmd>AV<CR>') --,          'edit alternate in tab'},
+    nnoremap('<leader>rat', '<cmd>AT<CR>') --,          'edit alternate in v split'
 
 
     -- Edits to "Relative" file in various locations
     nnoremap('<leader>re', '<cmd>R<CR>')
-    nnoremap('<leader>ree', '<cmd>RE<CR>') --, 'edit relative file'
-    nnoremap('<leader>res', '<cmd>RS<CR>') --, 'edit relative in h split'
-    nnoremap('<leader>rev', '<cmd>RV<CR>') --, 'edit relative in tab'
-    nnoremap('<leader>ret', '<cmd>RT<CR>') --, 'edit relative in v split'
+    nnoremap('<leader>ree', '<cmd>RE<CR>') --,          'edit relative file'
+    nnoremap('<leader>res', '<cmd>RS<CR>') --,          'edit relative in h split'
+    nnoremap('<leader>rev', '<cmd>RV<CR>') --,          'edit relative in tab'
+    nnoremap('<leader>ret', '<cmd>RT<CR>') --,          'edit relative in v split'
+
+
+    -- Partial Extraction
+    vnoremap('<leader>rex', ':Extract ') --,            'extract to partial'
+
+    -- Preview Page
+    nnoremap('<leader>rp', ':Preview<CR>') --,          'open web browser for current view'
+
+    -- Run Ctags for Ruby/Rails program, includes gems
+    nnoremap('<leader>rt', ':!ctags -R --languages=Ruby --exclude=.git --exclude=log . $(bundle list --paths) && echo "tags" >> .gitignore<CR>') --,  create ctags for project incl bundled gems
   end
 }
 
 return M
-
-
-
-
---    b = {':!bundle install<cr>', 'Bundle Install'},
---    name = '+rails',
---    e = {":Extract ",                                           'Extracts range to Partial, Helper, Concern'}
-
-
-
