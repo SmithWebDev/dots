cd6bac0befc39a3bf88a93251f1f5ea31f7845be
Darrick Smith <smithwebdd@gmail.com>
2021-10-22 00:53:03 -0400

    corrected Colorizer config file

--- nvim/lua/smithwebdev/plugins/config/colorizer.lua
+++ nvim/lua/smithwebdev/plugins/config/colorizer.lua
@@ -5,18 +5,20 @@ M.plugin = {
 
   after = 'mapx.nvim',
 
-  setup = function()
-    default_options = {
-      rgb      = true, -- #RGB hex codes
-      rrggbb   = true, -- #RRGGBB hex codes
-      names    = true, -- "Name" codes like Blue
-      rrggbbaa = true, -- #RRGGBBAA hex codes
-      rgb_fn   = true, -- CSS rgb() and rgba() functions
-      hsl_fn   = true, -- CSS hsl() and hsla() functions
-      css      = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
-      css_fn   = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
-      },
-  end,
+  config = function()
+    require('colorizer').setup({
+      default_options = {
+        rgb      = true, -- #RGB hex codes
+        rrggbb   = true, -- #RRGGBB hex codes
+        names    = true, -- "Name" codes like Blue
+        rrggbbaa = true, -- #RRGGBBAA hex codes
+        rgb_fn   = true, -- CSS rgb() and rgba() functions
+        hsl_fn   = true, -- CSS hsl() and hsla() functions
+        css      = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+        css_fn   = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
+      }
+    })
+  end
 }
 
 return M
