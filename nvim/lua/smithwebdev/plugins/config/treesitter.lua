e6af62bcd9476064ecbcd0d001663aeb91b22c35
Darrick Smith <smithwebdd@gmail.com>
2021-10-23 23:18:48 -0400

    added TSPlayground config to project. plugin setting added to treesitter file

--- nvim/lua/smithwebdev/plugins/config/treesitter.lua
+++ nvim/lua/smithwebdev/plugins/config/treesitter.lua
@@ -6,6 +6,8 @@ M.plugin = {
   -- Run this command after the plugin (nvim-treesitter) is loaded
   run = ':TSUpdate',
 
+  after = 'mapx.nvim',
+
   -- Setup nvim-treesitter settings AFTER the plugin (nvim-treesitter) is loaded
   config = function()
     require'nvim-treesitter.configs'.setup {
@@ -17,14 +19,31 @@ M.plugin = {
         'css',
         'javascript',
         'go',
-      },
+        'query', },
       highlight = {
         enable = true, -- false will disable the whole extension
       },
       indent = {
         enable = true,
       },
-    }
+      playground = {
+        enable = true,
+        disable = {},
+        updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
+        persist_queries = false, -- Whether the query persists across vim sessions
+        keybindings = {
+          toggle_query_editor = 'o',
+          toggle_hl_groups = 'i',
+          toggle_injected_languages = 't',
+          toggle_anonymous_nodes = 'a',
+          toggle_language_display = 'I',
+          focus_language = 'f',
+          unfocus_language = 'F',
+          update = 'R',
+          goto_node = '<cr>',
+          show_help = '?',},
+        }
+      }
   end,
 }
 
