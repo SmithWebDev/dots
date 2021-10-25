4f0da3861cdff437ddc1859ffead9472f79a4044
Darrick Smith <smithwebdd@gmail.com>
2021-10-24 22:46:52 -0400

    updated navigator to remove possible overrides applied by nvim-lsp-installer

--- nvim/lua/smithwebdev/plugins/config/navigator.lua
+++ nvim/lua/smithwebdev/plugins/config/navigator.lua
@@ -2,8 +2,6 @@ local M = {}
 
 M.plugin = {
   'ray-x/navigator.lua',
-  'williamboman/nvim-lsp-installer', -- https://github.com/williamboman/nvim-lsp-installer
-  'neovim/nvim-lspconfig',
 
   after = {
     'mapx.nvim',
@@ -13,15 +11,17 @@ M.plugin = {
   -- This plugin requires that the following plugins be loaded BEFORE navigator.lua
   requires = {
     'neovim/nvim-lspconfig',
+    'williamboman/nvim-lsp-installer', -- https://github.com/williamboman/nvim-lsp-installer
+    'neovim/nvim-lspconfig',
     { 'ray-x/guihua.lua', run = 'cd lua/fzy && make' },
   },
 
   -- Setup navigator.lua settings AFTER the plugin (navigator.lua) is loaded
   config = function()
     local capabilities = vim.lsp.protocol.make_client_capabilities()
-    capabilities.textDocument.completion.completionItem.snippetSupport = true
+    capabilities.textDocument.completion.completionItem.snippetSupport = true,
 
-    require'navigator'.setup({
+    require('navigator').setup({
       on_attach = function(client)
         if client.resolved_capabilities.document_formatting then
           vim.cmd [[
@@ -36,15 +36,15 @@ M.plugin = {
       lsp_installer = true,
     })
 
-    local lsp_installer = require('nvim-lsp-installer')
-    local lsp_installer_servers = require('nvim-lsp-installer.servers')
+    --local lsp_installer = require('nvim-lsp-installer')
+    --local lsp_installer_servers = require('nvim-lsp-installer.servers')
 
-    lsp_installer.on_server_ready(function(server)
-      local opts = {}
+    --lsp_installer.on_server_ready(function(server)
+    --  local opts = {}
 
-      server:setup(opts)
-      vim.cmd [[ do User LspAttachBuffers ]]
-    end)
+    --  server:setup(opts)
+    --  vim.cmd [[ do User LspAttachBuffers ]]
+    --end)
   end,
 }
 
