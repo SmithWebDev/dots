cd97530c5fb7e7e062e820ca96e89313f53b58e2
Darrick Smith <smithwebdd@gmail.com>
2021-10-29 17:05:46 -0400

    Updated mappings for ruby/rails filetype

--- nvim/lua/smithwebdev/plugins/config/rails.lua
+++ nvim/lua/smithwebdev/plugins/config/rails.lua
@@ -15,26 +15,26 @@ M.plugin = {
 
   config = function()
     -- keybindings
-    nnoremap('<leader>rba',          ':!bundle add<Space>') --,   Bundle Add Command
-    nnoremap('<leader>rbi',          ':!bundle install<CR>') --,  Bundle Install Command
-    nnoremap('<leader>rbu',          ':!bundle update<CR>') --,   Bundle Update Command
-    nnoremap('<leader>rc',           '<cmd>Console!<CR>') --,     Open Rails Console New Tmux Window
-    nnoremap('<leader>rs',           '<cmd>Server!<CR>') --,      Open Rails Server New Tmux Window
+    nnoremap('<leader><leader>rba',          ':!bundle add<Space>') --,   Bundle Add Command
+    nnoremap('<leader><leader>rbi',          ':!bundle install<CR>') --,  Bundle Install Command
+    nnoremap('<leader><leader>rbu',          ':!bundle update<CR>') --,   Bundle Update Command
+    nnoremap('<leader><leader>rc',           '<cmd>Console!<CR>') --,     Open Rails Console New Tmux Window
+    nnoremap('<leader><leader>rs',           '<cmd>Server!<CR>') --,      Open Rails Server New Tmux Window
 
 
-    nnoremap('<leader>rD',           ':Destroy<Space>') --,       Rails Destroy Command
-    nnoremap('<leader>rg',           ':Generate<Space>') --,      Rails Generate Command
-    nnoremap('<leader><leader>r',    '!rails<Space>') --,         Rails CLI command
+    nnoremap('<leader><leader>rD',           ':Destroy<Space>') --,       Rails Destroy Command
+    nnoremap('<leader><leader>rg',           ':Generate<Space>') --,      Rails Generate Command
+    nnoremap('<leader><leader><leader><leader>r',    '!rails<Space>') --,         Rails CLI command
 
-    nnoremap('<leader>rdbm',         '!rails db:migrate<CR>') --, Rails DB:Migrate Command
-    nnoremap('<leader>rdbs',         '!rails db:setup') --,       Rails DB:Setup Command
-    nnoremap('<leader>rdbS',         '!rails db:seed') --,        Rails DB:Seed Command
+    nnoremap('<leader><leader>rdbm',         '!rails db:migrate<CR>') --, Rails DB:Migrate Command
+    nnoremap('<leader><leader>rdbs',         '!rails db:setup') --,       Rails DB:Setup Command
+    nnoremap('<leader><leader>rdbS',         '!rails db:seed') --,        Rails DB:Seed Command
     --nnoremap('',                   '')
     --nnoremap('',                   '')
 
 
     -- Invoking :Rails with no arguments runs a default task.
-    nnoremap('<leader>rR',           '<cmd>Rails<CR>')
+    nnoremap('<leader><leader>rR',           '<cmd>Rails<CR>')
     -- File,                         Task ~
     -- test/*_test.rb,               test TEST=test/*_test.rb
     -- spec/*_spec.rb,               spec SPEC=spec/*_spec.rb
@@ -58,35 +58,35 @@ M.plugin = {
     -- database.yml,                 database.example.yml,        environments/*.rb
 
     -- Edits to "Alternative" file in various locations
-    nnoremap('<leader>ra',           '<cmd>A<CR>')
-    nnoremap('<leader>rae',          '<cmd>AE<CR>') --,           'edit alternate file'
-    nnoremap('<leader>ras',          '<cmd>AS<CR>') --,           'edit alternate in h split'
-    nnoremap('<leader>rav',          '<cmd>AV<CR>') --,           'edit alternate in tab'},
-    nnoremap('<leader>rat',          '<cmd>AT<CR>') --,           'edit alternate in v split'
+    nnoremap('<leader><leader>ra',           '<cmd>A<CR>')
+    nnoremap('<leader><leader>rae',          '<cmd>AE<CR>') --,           'edit alternate file'
+    nnoremap('<leader><leader>ras',          '<cmd>AS<CR>') --,           'edit alternate in h split'
+    nnoremap('<leader><leader>rav',          '<cmd>AV<CR>') --,           'edit alternate in tab'},
+    nnoremap('<leader><leader>rat',          '<cmd>AT<CR>') --,           'edit alternate in v split'
 
 
     -- Edits to "Relative" file in various locations
-    nnoremap('<leader>re',           '<cmd>R<CR>')
-    nnoremap('<leader>ree',          '<cmd>RE<CR>') --,           'edit relative file'
-    nnoremap('<leader>res',          '<cmd>RS<CR>') --,           'edit relative in h split'
-    nnoremap('<leader>rev',          '<cmd>RV<CR>') --,           'edit relative in tab'
-    nnoremap('<leader>ret',          '<cmd>RT<CR>') --,           'edit relative in v split'
+    nnoremap('<leader><leader>re',           '<cmd>R<CR>')
+    nnoremap('<leader><leader>ree',          '<cmd>RE<CR>') --,           'edit relative file'
+    nnoremap('<leader><leader>res',          '<cmd>RS<CR>') --,           'edit relative in h split'
+    nnoremap('<leader><leader>rev',          '<cmd>RV<CR>') --,           'edit relative in tab'
+    nnoremap('<leader><leader>ret',          '<cmd>RT<CR>') --,           'edit relative in v split'
 
 
     -- Partial Extraction
-    vnoremap('<leader>rex',          ':Extract ') --,             'extract to partial'
+    vnoremap('<leader><leader>rex',          ':Extract ') --,             'extract to partial'
 
     -- Rfactory
-    nnoremap('<leader>rf',           '<cmd>Rfactory<CR>') --,     'Edit Factory File'
-    nnoremap('<leader>rfs',          '<cmd>RSfactory<CR>') --,    'Edit Factory File in H Split'
-    nnoremap('<leader>rfv',          '<cmd>RVfactory<CR>') --,    'Edit Factory File in V Split'
-    nnoremap('<leader>rft',          '<cmd>RTfactory<CR>') --,    'Edit Factory File in Tab'
+    nnoremap('<leader><leader>rf',           '<cmd>Rfactory<CR>') --,     'Edit Factory File'
+    nnoremap('<leader><leader>rfs',          '<cmd>RSfactory<CR>') --,    'Edit Factory File in H Split'
+    nnoremap('<leader><leader>rfv',          '<cmd>RVfactory<CR>') --,    'Edit Factory File in V Split'
+    nnoremap('<leader><leader>rft',          '<cmd>RTfactory<CR>') --,    'Edit Factory File in Tab'
 
     -- Preview Page
-    nnoremap('<leader>rp',           ':Preview<CR>') --,          'open web browser for current view'
+    nnoremap('<leader><leader>rp',           ':Preview<CR>') --,          'open web browser for current view'
 
     -- Run Ctags for Ruby/Rails program, includes gems
-    nnoremap('<leader>rt',               ':!ctags -R --languages=Ruby --exclude=.git --exclude=log . $(bundle list --paths) && echo "tags" >> .gitignore<CR>') --,  create ctags for project incl bundled gems
+    nnoremap('<leader><leader>rt',               ':!ctags -R --languages=Ruby --exclude=.git --exclude=log . $(bundle list --paths) && echo "tags" >> .gitignore<CR>') --,  create ctags for project incl bundled gems
   end
 }
 
