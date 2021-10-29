b7992fe1eb541624279a62ae26e65437e772036c
Darrick Smith <smithwebdd@gmail.com>
2021-10-29 17:55:06 -0400

    initial update of navigator keybinds...WIP

--- nvim/lua/smithwebdev/plugins/config/navigator.lua
+++ nvim/lua/smithwebdev/plugins/config/navigator.lua
@@ -12,6 +12,7 @@ M.plugin = {
   requires = {
     'neovim/nvim-lspconfig',
     'williamboman/nvim-lsp-installer', -- https://github.com/williamboman/nvim-lsp-installer
+    'ray-x/lsp_signature.nvim',        -- https://github.com/ray-x/lsp_signature.nvim
     { 'ray-x/guihua.lua', run = 'cd lua/fzy && make' },
   },
 
@@ -19,6 +20,8 @@ M.plugin = {
   config = function()
     local capabilities = vim.lsp.protocol.make_client_capabilities()
     capabilities.textDocument.completion.completionItem.snippetSupport = true,
+
+    require('lsp_signature').setup(),
 
     require('navigator').setup({
       on_attach = function(client)
@@ -35,41 +38,44 @@ M.plugin = {
       lsp_installer = true,
       default_mapping = false,
       keymaps = {
-        --{key  = "gr",            func = "require('navigator.reference').reference()"},
-        --{mode = "i",             key  = "<M-k>", func = "signature_help()"},
-        --{key  = "<c-k>",         func = "signature_help()"},
-        --{key  = "g0",            func = "require('navigator.symbols').document_symbols()"},
-        --{key  = "gW",            func = "workspace_symbol()"},
-        --{key  = "<c-]>",         func = "require('navigator.definition').definition()"},
-        --{key  = "gD",            func = "declaration({ border = 'rounded', max_width = 80 })"},
-        --{key  = "gp",            func = "require('navigator.definition').definition_preview()"},
-        --{key  = "gT",            func = "require('navigator.treesitter').buf_ts()"},
-        --{key  = "<Leader>gT",    func = "require('navigator.treesitter').bufs_ts()"},
-        --{key  = "K",             func = "hover({ popup_opts = { border = single, max_width = 80 }})"},
-        --{key  = "<Space>ca",     mode = "n", func = "require('navigator.codeAction').code_action()"},
-        --{key  = "<Space>cA",     mode = "v", func = "range_code_action()"},
-        --{key  = "<Leader>re",    func = "rename()"},
-        --{key  = "<Space>rn",     func = "require('navigator.rename').rename()"},
-        --{key  = "<Leader>gi",    func = "incoming_calls()"},
-        --{key  = "<Leader>go",    func = "outgoing_calls()"},
-        --{key  = "gi",            func = "implementation()"},
-        --{key  = "<Space>D",      func = "type_definition()"},
-        --{key  = "gL",            func = "require('navigator.diagnostics').show_diagnostics()"},
-        --{key  = "gG",            func = "require('navigator.diagnostics').show_buf_diagnostics()"},
-        --{key  = "]d",            func = "diagnostic.goto_next({ border = 'rounded', max_width = 80})"},
-        --{key  = "[d",            func = "diagnostic.goto_prev({ border = 'rounded', max_width = 80})"},
-        --{key  = "]r",            func = "require('navigator.treesitter').goto_next_usage()"},
-        --{key  = "[r",            func = "require('navigator.treesitter').goto_previous_usage()"},
-        --{key  = "<C-LeftMouse>", func = "definition()"},
-        --{key  = "g<LeftMouse>",  func = "implementation()"},
-        --{key  = "<Leader>k",     func = "require('navigator.dochighlight').hi_symbol()"},
-        --{key  = '<Space>wa',     func = 'add_workspace_folder()'},
-        --{key  = '<Space>wr',     func = 'remove_workspace_folder()'},
-        --{key  = '<Space>ff',     func = 'formatting()', mode='n'},
-        --{key  = '<Space>ff',     func = 'range_formatting()', mode='v'},
-        --{key  = '<Space>wl',     func = 'print(vim.inspect(vim.lsp.buf.list_workspace_folders()))'},
-        --{key  = "<Space>la",     mode = "n", func = "require('navigator.codelens').run_action()"},
+        {key  = "<leader>lca",    mode = "n", func = "require('navigator.codeAction').code_action()"},
+        {key  = "<leader>lcl",    mode = "n", func = "require('navigator.codelens').run_action()"},
+        {key  = "<leader>ldp",    func = "require('navigator.definition').definition_preview()"},
+        {key  = "<leader>lgd",    func = "require('navigator.diagnostics').show_diagnostics()"},
+        {key  = "<leader>lbd",    func = "require('navigator.diagnostics').show_buf_diagnostics()"},
+        --{key  = "<Leader>k",    func = "require('navigator.dochighlight').hi_symbol()"},
+        {key  = "<leader>lrf",    func = "require('navigator.reference').reference()"},
+        {key  = "<leader>lrn",    func = "require('navigator.rename').rename()"},
+        {key  = "<leader>lds",    func = "require('navigator.symbols').document_symbols()"},
+        {key  = "<leader>lbT",    func = "require('navigator.treesitter').buf_ts()"},
+        --{key  = "<Leader>gT",   func = "require('navigator.treesitter').bufs_ts()"},
+        {key  = "<leader>lnn",    func = "require('navigator.treesitter').goto_next_usage()"},
+        {key  = "<leader>lpp",    func = "require('navigator.treesitter').goto_previous_usage()"},
+
+        {key  = "<leader>lsh",    func = "signature_help()"},
+        --{key  = "gW",           func = "workspace_symbol()"},
+        {key  = "<leader>lD",     func = "declaration({ border = 'rounded', max_width = 80 })"},
+        {key  = "K",              func = "hover({ popup_opts = { border = single, max_width = 80 }})"},
+        {key  = "<leader>lca",    mode = "v", func = "range_code_action()"},
+        --{key  = "<leader>lrn",  func = "rename()"},
+        --{key  = "<leader>gi",   func = "incoming_calls()"},
+        --{key  = "<leader>go",   func = "outgoing_calls()"},
+        {key  = "<leader>li",     func = "implementation()"},
+        --{key  = "<leader>D",    func = "type_definition()"},
+        {key  = "<leader>ldn",    func = "diagnostic.goto_next({ border = 'rounded', max_width = 80})"},
+        {key  = "<leader>ldp",    func = "diagnostic.goto_prev({ border = 'rounded', max_width = 80})"},
+        {key  = "<leader>ldf",    func = "definition()"},
+        --{key  = "g<LeftMouse>", func = "implementation()"},
+        --{key  = '<leader>wa',   func = 'add_workspace_folder()'},
+        --{key  = '<leader>wr',   func = 'remove_workspace_folder()'},
+        {key  = '<leader>lm',     func = 'formatting()', mode='n'},
+        {key  = '<leader>lm',     func = 'range_formatting()', mode='v'},
+        {key  = '<leader>lw',     func = 'print(vim.inspect(vim.lsp.buf.list_workspace_folders()))'},
+        --{key  = "<M-k>",        mode = "i", func = "signature_help()"},
       },
+      html = {
+        filetype = {'erb'}
+          }
     })
 
     --require('lspconfig').efm.setup({
