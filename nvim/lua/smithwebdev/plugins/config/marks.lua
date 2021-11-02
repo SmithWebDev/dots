2ad5c3f59ccd6d9b6c8081cf22d6fffaffb6993f
Darrick Smith <smithwebdd@gmail.com>
2021-11-01 23:39:23 -0400

    added cleaned up marks config

--- nvim/lua/smithwebdev/plugins/config/marks.lua
+++ nvim/lua/smithwebdev/plugins/config/marks.lua
@@ -7,9 +7,9 @@ M.plugin = {
 
   config = function()
     require('marks').setup {
-    default_mappings = true,
-    signs = true,
-    mappings = {}
+      default_mappings = true,
+      signs = true,
+      mappings = {}
     }
   end,
 }
@@ -26,8 +26,7 @@ return M
 -- m]              Move to next mark
 -- m[              Move to previous mark
 -- m:              Preview mark. This will prompt you for a specific mark to
---                 preview; press <cr> to preview the next mark.
---                     
+------------------ preview; press <cr> to preview the next mark.
 -- m[0-9]          Add a bookmark from bookmark group[0-9].
 -- m[0-9]          Delete all bookmarks from bookmark group[0-9].
 -- m}              Move to the next bookmark having the same type as the bookmark under
@@ -39,23 +38,13 @@ return M
 
 -- Highlights and Commands
 -- ========================================
--- 
 -- MarkSignHL                   The highlight group for displayed mark signs.
--- 
 -- MarkSignNumHL                The highlight group for the number line in a signcolumn.
--- 
 -- MarkVirtTextHL               The highlight group for bookmark virtual text annotations.
--- 
 -- marks.nvim                   also defines the following commands:
--- 
 -- :MarksToggleSigns[buffer]    Toggle signs globally. Also accepts an optional buffer number to toggle signs for that buffer only.
--- 
 -- :MarksListBuf                Fill the location list with all marks in the current buffer.
--- 
 -- :MarksListGlobal             Fill the location list with all global marks in open buffers.
--- 
 -- :MarksListAll                Fill the location list with all marks in all open buffers.
--- 
 -- :BookmarksList group_number  Fill the location list with all bookmarks of group "group_number".
--- 
 -- :BookmarksListAll            Fill the location list with all bookmarks, across all groups.
