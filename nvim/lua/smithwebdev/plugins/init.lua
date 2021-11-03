ef9ebdda13f6907461fee249b68235ba269889e6
Darrick Smith <smithwebdd@gmail.com>
2021-11-03 18:34:10 -0400

    Added hlslens, animate with lens, and vim-smoothie for editor aesthetics, cleaned up plugins/init file

--- nvim/lua/smithwebdev/plugins/init.lua
+++ nvim/lua/smithwebdev/plugins/init.lua
@@ -18,58 +18,61 @@ packer.init {
 
 local plugins = {
   -- Aesthetics
+  'animate',               -- https://github.com/camspiers/animate.vim
   'colorschemes',
+  'vim_smoothie',          -- https://github.com/psliwka/vim-smoothie
 
   -- Completion
   --'coq',                 -- https://github.com/ms-jpq/coq_nvim  **
-  'cmp',               -- https://github.com/hrsh7th/nvim-cmp
-  'emmet',               -- https://github.com/mattn/emmet-vim
-  'tags',              -- https://github.com/ludovicchabant/vim-gutentags  **
+  'cmp',                   -- https://github.com/hrsh7th/nvim-cmp
+  'emmet',                 -- https://github.com/mattn/emmet-vim
+  'tags',                  -- https://github.com/ludovicchabant/vim-gutentags  **
 
   -- Editing
-  'autopairs', -- https://github.com/windwp/nvim-autopairs
-  'colorizer',           -- https://github.com/norcalli/nvim-colorizer.lua
-  'comment', -- https://github.com/numToStr/Comment.nvim
-  'easy_align',          -- https://github.com/junegunn/vim-easy-align
-  --'lualine', -- https://github.com/hoob3rt/lualine.nvim
-  'marks',               -- https://github.com/chentau/marks.nvim
-  'surround',            -- https://github.com/tpope/vim-surround
-  --'speeddating',       -- https://github.com/tpope/vim-speeddating
-  --'heroku',            -- https://github.com/tpope/vim-heroku
+  'autopairs',             -- https://github.com/windwp/nvim-autopairs
+  'colorizer',             -- https://github.com/norcalli/nvim-colorizer.lua
+  'comment',               -- https://github.com/numToStr/Comment.nvim
+  'easy_align',            -- https://github.com/junegunn/vim-easy-align
+  'hlslens',               -- https://github.com/kevinhwang91/nvim-hlslens
+  --'lualine',             -- https://github.com/hoob3rt/lualine.nvim
+  'marks',                 -- https://github.com/chentau/marks.nvim
+  'surround',              -- https://github.com/tpope/vim-surround
+  --'speeddating',         -- https://github.com/tpope/vim-speeddating
+  --'heroku',              -- https://github.com/tpope/vim-heroku
 
   -- Filetypes
   --'dash',                -- https://github.com/mrjones2014/dash.nvim  **
-  --'haml',              -- https://github.com/tpope/vim-haml
-  'rails',               -- https://github.com/tpope/vim-rails  **
+  --'haml',                -- https://github.com/tpope/vim-haml
+  'rails',                 -- https://github.com/tpope/vim-rails  **
   --'seeing_is_believing', -- https://github.com/JoshCheek/seeing_is_believing
 
   -- Fuzzy / File Finder
-  'fzy',                 -- https://github.com/mfussenegger/nvim-fzy  **
-  'telescope',           -- https://github.com/nvim-telescope/telescope.nvim  **
+  'fzy',                   -- https://github.com/mfussenegger/nvim-fzy  **
+  'telescope',             -- https://github.com/nvim-telescope/telescope.nvim  **
 
   -- Git
-  'lazygit',             -- https://github.com/kdheepak/lazygit.nvim **
+  'lazygit',               -- https://github.com/kdheepak/lazygit.nvim **
 
   -- Keybinds
-  'mapx',                -- https://github.com/b0o/mapx.nvim
-  'whichkey',          -- https://github.com/folke/which-key.nvim
+  'mapx',                  -- https://github.com/b0o/mapx.nvim
+  'whichkey',              -- https://github.com/folke/which-key.nvim
 
   -- LSP & File Navigation
-  'CHADtree', -- https://github.com/ms-jpq/chadtree
-  'navigator',           -- https://github.com/ray-x/navigator.lua  **
+  'CHADtree',              -- https://github.com/ms-jpq/chadtree
+  'navigator',             -- https://github.com/ray-x/navigator.lua  **
 
   -- Organization Utilities
-  'orgmode',             -- https://github.com/kristijanhusak/orgmode.nvim
+  'orgmode',               -- https://github.com/kristijanhusak/orgmode.nvim
 
   -- Snippets
-  'ultisnips', -- https://github.com/SirVer/ultisnips
+  'ultisnips',             -- https://github.com/SirVer/ultisnips
 
   -- Testing
-  'ultest',              -- https://github.com/rcarriga/vim-ultest  **
+  'ultest',                -- https://github.com/rcarriga/vim-ultest  **
 
   -- Treesitter/Syntax Highlighting
-  'playground',          -- https://github.com/nvim-treesitter/playground  **
-  'treesitter',          -- https://github.com/nvim-treesitter/nvim-treesitter
+  'playground',            -- https://github.com/nvim-treesitter/playground  **
+  'treesitter',            -- https://github.com/nvim-treesitter/nvim-treesitter
 }
 
 packer.startup(function(use)
