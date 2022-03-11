fd049555503a678dfbbd38e27d79946c8e290991
SmithWebDev <smithwebdd@gmail.com>
2022-03-11 15:13:36 -0500

    cleaned up autocmds

--- nvim/lua/smithwebdev/core/autocmds.lua
+++ nvim/lua/smithwebdev/core/autocmds.lua
@@ -2,43 +2,35 @@
 --                 SmithWebDev                 --
 --                  AutoCmds                   --
 ---[[---------------------------------------]]---
-local auto = vim.api.nvim_exec
+local auto = vim.cmd
 
 -- Automatically exit from Insert mode
-auto(
-  [[
+auto [[
     augroup exitInsert
       au CursorHoldI * stopinsert
       au InsertEnter * let updaterestore=&updatetime | set updatetime=2500
       au InsertLeave * let &updatetime=updaterestore
     augroup END
-  ]], false
-  )
+  ]]
 
 -- Remove trailing whitespace on save
-auto(
-  [[
+auto [[
     autocmd BufWritePre * :%s/\s\+$//e
-  ]], false
-  )
+  ]]
 
 -- Help files open to the right of the current buffer
-auto(
-  [[
+auto [[
     autocmd Filetype help wincmd L
-  ]], false
-  )
+  ]]
 
 -- Saves on text change or exit Insert mode
-auto(
-  [[
+auto [[
     augroup autosave
         au!
         let blacklist = ['packer', 'netrw', 'TelescopePrompt', 'lspinfo', 'lsp-installer', 'query', 'tsplayground', 'text', 'harpoon', 'scratch']
         au BufEnter * if &filetype == "" | setlocal ft=text | endif
-        au TextChanged,InsertLeave * if index(blacklist, &ft) < 0 && &modifiable| silent w | endif
+        au TextChanged,InsertLeave * if index(blacklist, &ft) < 0 && &modifiable && &buftype == ''| silent w | endif
     augroup END
-  ]], false
-  )
+  ]] 
 
 -- let blacklist = ['packer', 'NvimTree', 'netrw', 'TelescopePrompt', 'spectre_panel', 'startify', 'calendar', 'text', 'help', 'Trouble', 'tutor', 'Neoformat', 'lspinfo', 'any-jump', 'tsplayground', 'orgagenda', 'gina-branch', 'lazygit','LspSagaCodeAction']
